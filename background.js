63513dd92eef9160ecf80fddc36b9c962e67543b
Maxcafe <universe.r.0805@gmail>
2019-09-25 19:29:33 +0900

    思いつかない

--- background.js
+++ background.js
@@ -3,7 +3,7 @@ var pattern = "https://developer.mozilla.org/*";
 function redirect(requestDetails) {
   console.log("Redirecting: " + requestDetails.url);
   return {
-    redirectUrl: "https://www.mozilla.org/en-US/privacy/firefox/"
+    redirectUrl: "https://raw.githubusercontent.com/akazs/MWS2019_F.SE/kawakami/alert.html?token=AIER2Z74R3HS53ZBK56763C5SR2S6"
   };
 }
 
