3ef8af12212cb2f69efce5cb1e103151604fb23a
wanidon <wwwanidon@gmail.com>
2019-09-25 15:54:02 +0900

    とりあえず動くもの

---
+++ background.js
@@ -0,0 +1,11 @@
+document.body.style.border = "5px solid red";
+console.log('hoge');
+// backgroundでは使えない
+alert("Hello world!"); 
+// const hoge = 'hogee';
+
+
+// while (true){
+//     console.log('hoge');
+// }
+
