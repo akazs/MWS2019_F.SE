1b7eb8fb7fce939f615a284aeb159f6c0c71422d
岩花一輝 <iwahanakazuki@iwahanaikkinoMacBook-puro.local>
2019-09-25 20:56:04 +0900

    redirect機能

--- background.js
+++ background.js
@@ -1,11 +1,25 @@
-document.body.style.border = "5px solid red";
-console.log('hoge');
-// alertはbackgroundでは使えないのでデバッグ用
-alert("Hello world!"); 
-// const hoge = 'hogee';
+var pattern ="https://www.mediaopusplus.com/";
 
+function check_http(original_url){
+	console.log(original_url);
+	var splited_url = original_url.split(":");
+	console.log(splited_url[0]);
+	if (splited_url[0] == "http"){
+		return true;
+	}
+	return false;
+}
 
-// while (true){
-//     console.log('hoge');
-// }
+function redirect(requestDetails){
+	var result = check_http(requestDetails.url);
+	console.log(result);
+	if (result){
+		return {redirectUrl: "https://38.media.tumblr.com/tumblr_ldbj01lZiP1qe0eclo1_500.gif"};
+	}
+}
 
+browser.webRequest.onBeforeRequest.addListener(
+  redirect,
+  {urls:[pattern]},
+  ["blocking"]
+);
