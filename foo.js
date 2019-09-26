7c0a74c439e94204d897e8cfd9e4e13742d7b7e8
Cheng Ju Chien <akaz@Cheng-Ju-ChiennoMacBook-Pro.local>
2019-09-26 13:46:31 +0900

    add function for fetching param and redirecting

--- foo.js
+++ foo.js
@@ -1,4 +1,19 @@
-function back1() {window.history.go(-1)}
+function getParam(name) {
+	name = name.replace(/[\[\]]/g, "\\$&");
+    var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+        results = regex.exec(url);
+    if (!results) return null;
+    if (!results[2]) return '';
+    return decodeURIComponent(results[2].replace(/\+/g, " "));
+}
+
+function back1() {window.history.go(-1);}
+function go() {window.location.href = getParam('to');}
+
 document.querySelector("#return").addEventListener('click', () => {
-    back1()
+    back1();
+})
+
+document.querySelector("#go").addEventListener('click', () => {
+	go();
 })
