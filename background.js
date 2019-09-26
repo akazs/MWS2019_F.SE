35f59b524301b731a85fd6de1c27f88697355e1a
wanidon <wwwanidon@gmail.com>
2019-09-26 12:12:47 +0900

    出来た

---
+++ background.js
@@ -0,0 +1,28 @@
+const redirectDest = chrome.extension.getURL("foo.html");
+blacklist = genBlackList()
+
+function get_scheme(original_url){
+	return original_url.split(":")[0];
+}
+
+function redirect(requestDetails){
+	var u = redirectDest
+	var scheme = get_scheme(requestDetails.url);
+	// http
+	if (scheme == 'http'){
+		return {redirectUrl: u};
+	// https
+	}else if (scheme == 'https') {
+		// ブラックリストにドメイン名があったとき
+		if (ret = search(blacklist,requestDetails.url)){
+			console.log("dangerou!: ",ret);
+			return {redirectUrl: u};
+		}
+	}
+}
+
+browser.webRequest.onBeforeRequest.addListener(
+  redirect,
+  {urls:["*://*/*"]},
+  ["blocking"]
+);
--- search.js
+++ search.js
@@ -1,29 +1,33 @@
-const blacklistDir = 'blacklist'
-const blaklistnames = ['ds1.json','ds2oth.json','ds3mal.json','ds3phi.json','ds3spa.json'];
-var blacklists = [];
-//var keyword = 'banner.titancasino.com';
-//var keyword = 'c.b.a.com';
+function genBlackList(){
+    const blacklistDir = 'blacklist'
+    const blaklistnames = ['ds1.json','ds2oth.json','ds3mal.json','ds3phi.json','ds3spa.json'];
+    var blacklists = [];
+    //var keyword = 'banner.titancasino.com';
+    //var keyword = 'c.b.a.com';
 
-for(var i=0;i<blaklistnames.length;i++){
-    httpObj = new XMLHttpRequest();
-    httpObj.open("get", blacklistDir+'/'+blaklistnames[i], true);
-    httpObj.onload = function(){
-            bl = JSON.parse(this.responseText);
-            console.log(bl);
-            blacklists.push(bl);
-        }
-    httpObj.send(null);
+    for(var i=0;i<blaklistnames.length;i++){
+        httpObj = new XMLHttpRequest();
+        httpObj.open("get", blacklistDir+'/'+blaklistnames[i], true);
+        httpObj.onload = function(){
+                bl = JSON.parse(this.responseText);
+                console.log(bl);
+                blacklists.push(bl);
+            }
+        httpObj.send(null);
+    }
+    return blacklists;
 }
 
-function genDomains(domain){
-    var splited = domain.split('.');
+function genDomains(url){
+    var splited = url.split('/')[2].split('.');
     var domains = []
     for(var i=0;i<splited.length-1;i++)
         domains.push(splited.slice(i,splited.length).join('.'));
     return domains;
 }
 
-function serch(blacklists,keywords){
+function search(blacklists,domain){
+    var keywords = genDomains(domain);
     for(var i=0;i<blacklists.length;i++){
         for (var j=0;j<keywords.length;j++)
             if (blacklists[i][keywords[j]])
