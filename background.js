2c9865606b1ea11b77d1e167005184c704b217d2
wanidon <wwwanidon@gmail.com>
2019-09-30 16:38:40 +0900

    Remove extra processing

--- background.js
+++ background.js
@@ -7,14 +7,10 @@ function get_scheme(original_url){
 }
 
 function redirect(requestDetails){
-	//checkSuspiciousDomain(requestDetails.url,requestDetails.requestId)
 	var u = redirectDest + '?to=' + requestDetails.url;
 	var u_http = redirectDest_http + '?to=' + requestDetails.url;
-	//var u = redirectDest;
 	var scheme = get_scheme(requestDetails.url);
-	//console.log(requestDetails.url);
 	if(searchTmpWhitelist(requestDetails.url))
-		//deleteTmpWhitelist(requestDetails.url);
 		console.log('approved by tmp white list :',requestDetails.url);
 	else{
 		if (ret = search(blacklists,requestDetails.url)){
@@ -22,7 +18,7 @@ function redirect(requestDetails){
 			console.log("url: ", requestDetails.url);
 			return {redirectUrl: u};
 		} else{
-			//ブラックリストにあるかも？？
+
 			if (check_blacklist_posibility(requestDetails.url.split("/")[2])){
 				if (!search(whitelists,requestDetails.url)){
 					return {redirectUrl: u};
@@ -31,22 +27,20 @@ function redirect(requestDetails){
 				if (scheme == 'http'){
 					return {redirectUrl: u_http};
 				} else{
-					////
 					browser.webRequest.onHeadersReceived.addListener(
                 async function(details) {
                     let securityInfo = await browser.webRequest.getSecurityInfo(
                         details.requestId,
                         {"certificateChain": false}
                     )
-                    //console.log(JSON.stringify(securityInfo['certificates']))
+
                     let validity = securityInfo['certificates'][0]['validity']
                     let days = (validity['end'] - validity['start']) / (1000 * 86400)
                     console.log(days, 'Days')
                     if (days <= 90) {
-											if (!search(whitelists,requestDetails.url)){
-												return {redirectUrl: u};
-											}
-                        //return {redirectUrl: u};
+						if (!search(whitelists,requestDetails.url)){
+							return {redirectUrl: u};
+						}
                     }
                 },
                 {urls:["*://*/*"],types:["main_frame"]},
@@ -64,35 +58,3 @@ browser.webRequest.onBeforeRequest.addListener(
   {urls:["*://*/*"],types:["main_frame"]},
   ["blocking"]
 );
-//["image"]
-
-//　証明書情報取得の実験用
-async function logRootCert(details) {
-	try {
-	  let securityInfo = await browser.webRequest.getSecurityInfo(
-		details.requestId,
-		{"certificateChain": true}
-	  );
-	  console.log('onHeadersReceived, ',securityInfo)
-	  if ((securityInfo.state == "secure" || securityInfo.state == "weak") &&
-		  !securityInfo.isUntrusted) {
-		let rootName = securityInfo.certificates[securityInfo.certificates.length - 1].subject;
-		if (rootCertStats[rootName] === undefined) {
-		  rootCertStats[rootName] = 1;
-		} else {
-		  rootCertStats[rootName] = rootCertStats[rootName] + 1;
-		}
-	  }
-	}
-	catch(error) {
-	  console.error(error);
-	}
-  }
-
-  /*
-  Listen for all onHeadersReceived events.
-  */
-  browser.webRequest.onHeadersReceived.addListener(logRootCert,
-	{urls: ["<all_urls>"]},
-	["blocking"]
-  );
