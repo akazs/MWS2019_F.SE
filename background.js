3bcae80a8e20d5116b0af213326cfbaefca228bb
wanidon <wwwanidon@gmail.com>
2019-09-30 16:01:22 +0900

    testing

--- background.js
+++ background.js
@@ -16,6 +16,10 @@ function redirect(requestDetails){
 			return {redirectUrl: u};
 		// https
 		}else if (scheme == 'https') {
+			// window.location.replace('https://google.com');
+			// window.location.href = "https://google.com";
+			// window.location.assign("https://google.com");
+			// window.location = 'https://google.com'
 			// ブラックリストにドメイン名があったとき
 			if (ret = search(blacklists,requestDetails.url)){
 				console.log("dangerou!: ",ret);
--- keras.py
+++ keras.py
@@ -7,6 +7,6 @@ from tensorflow.keras.layers import Dense, Input
 blackdata = np.array(list(map(lambda S:[ord(S[i]) for i in range(len(S))],json.load( open('blacklist/ds3spa.json')).keys())))
 print(blackdata[0:10][:10])
 
-with open('whitelist/AlexaWhite.json') as f:
-    whiltedict = json.load(f)
-whitedata = np.array(list(map(lambda x: ord(x[0])/127,whiltedict.keys())))
+whitedata = np.array(list(map(lambda S:[ord(S[i]) for i in range(len(S))],json.load( open('whitelist/AlexaWhite.json')).keys())))
+
+print(whitedata[:10][:10])
